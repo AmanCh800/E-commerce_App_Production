fadb9d6310933b00ca2a7d3646bb8c09c76a8614
Aman Chadha <76766472+AmanCh800@users.noreply.github.com>
2024-01-02 18:03:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ import cors from "cors";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
